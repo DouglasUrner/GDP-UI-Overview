243c0b0470a0a0ab5e2f0e1bb85db66d480ee208
Douglas Urner <dlu@canishe.com>
2018-04-20 08:50:25 -0700

    Add ButtonTest script.

---
+++ UI-Overview/Assets/Scripts/ButtonTest.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonTest : MonoBehaviour {
+
+	public void OnButton() {
+		Debug.Log("Button was pressed!");
+	}
+}
